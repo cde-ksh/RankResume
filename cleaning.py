93dd4ade5499f9213dfea6b96c3d384f92bc5b48
Silly Duck <kapoor.kshiru22@gmail.com>
2026-03-15 19:57:08 +0530

    extraction logic

---
+++ cleaning.py
@@ -0,0 +1,2 @@
+import ftfy
+import re
---
+++ extractor.py
@@ -0,0 +1,47 @@
+import os
+import fitz
+import pdfplumber
+from docx import Document
+
+# getting the extension type of the file
+
+file = "/Users/kshiraj/Downloads/deterministic-resume-intelligence-main 2/data/sample/10089434.pdf"
+ext = os.path.splitext(file)[1].lower()
+
+# extracting text from pdf files 
+
+if ext == '.pdf':
+    with pdfplumber.open(file) as pdf:
+
+        with open("output.txt","w",encoding="utf8") as text:
+             
+            for page in pdf.pages:
+                content = page.extract_text()   
+
+                if content:
+                    text.write(content)
+                tables = page.extract_tables()
+
+                for table in tables:
+                    for row in table:
+                        row_text = " | ".join(str(cell) for cell in row if cell)
+                        text.write(row_text + "\n")
+
+# extracting files from a doc/docx file
+
+if ext == '.docx':
+    doc = Document(file)
+
+    with open("output.txt","w", encoding="utf8") as f:
+        for para in doc.paragraphs:
+            f.write(para.text + "\n")
+
+        for table in doc.tables:
+            for row in table.rows:
+                row_text = " | ".join(cell.text for cell in row.cells)
+                f.write(row_text + "\n")
+
+
+
+# reading the extracted text from the 
+
