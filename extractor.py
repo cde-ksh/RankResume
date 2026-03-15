00f3ee0b8a19f360e21b1d71010b92875bed4bda
Silly Duck <kapoor.kshiru22@gmail.com>
2026-03-15 23:37:18 +0530

    Add doc file extraction logic

--- extractor.py
+++ extractor.py
@@ -2,6 +2,7 @@ import os
 import fitz
 import pdfplumber
 from docx import Document
+import pypandoc
 
 # getting the extension type of the file
 
@@ -26,8 +27,9 @@ if ext == '.pdf':
                     for row in table:
                         row_text = " | ".join(str(cell) for cell in row if cell)
                         text.write(row_text + "\n")
-
-# extracting files from a doc/docx file
+                
+                     
+# extracting data from a docx file
 
 if ext == '.docx':
     doc = Document(file)
@@ -42,6 +44,31 @@ if ext == '.docx':
                 f.write(row_text + "\n")
 
 
+# extracting files from a doc file
+
+if ext == '.doc':
+    doc_file = file + "x"
+    
+    pypandoc.convert_file(file,"docx",outputfile=doc_file)
+
+    doc = Document(doc_file)
+
+    with open("output.txt","w", encoding="utf8") as f:
+        for para in doc.paragraphs:
+            f.write(para.text + "\n")
+
+        for table in doc.tables:
+            for row in table.rows:
+                row_text = " | ".join(cell.text for cell in row.cells)
+                f.write(row_text + "\n")
+
 
 # reading the extracted text from the 
 
+print("\n <--- Extracted text: ---> \n")
+
+with open("output.txt","r") as f:
+    data = f.readlines()
+    for line in data:
+        print(line)
+print("\n <--- Extraction Ended ---> \n")
