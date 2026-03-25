a6a3671d4c2ac3014424c949eb38f07914dcd71d
Silly Duck <itsmekshka@gmail.com>
2026-03-25 21:22:35 +0530

    improving cleaning pipeline

--- extraction/cleaning.py
+++ extraction/cleaning.py
@@ -1,22 +1,27 @@
 import ftfy
 import re
+import unicodedata
 
 def clean_text(text):
 
+    # unicode
+    text = unicodedata.normalize("NFKD", text)
+    text = text.encode('ascii','ignore').decode()
+
     # remove extra spaces
-    text = re.sub(r' \t+', ' ', text)
+    text = re.sub(r'[ \t+]', ' ', text)
 
     # normalising the spaces
-    text = re.sub(r'\s+', " ", text).strip()
+    text = re.sub(r'[^\S\n]+', " ", text).strip()
 
     # fixing Uppercase with spaces
-    text = re.sub(r'(\b[A-Z])\s+(?=[A-Z]\b)', 'r\1', text)
+    text = re.sub(r'(\b[A-Z])\s+(?=[A-Z]\b)', r'\1', text)
 
     # spaces before punchuation
-    text = re.sub(r'\s+(,.)', r'\1', text)
+    text = re.sub(r'\s+([,.;:])', r'\1', text)
 
     # remove unicode 
-    text = re.sub(r'[^a-zA-Z0-9@.+\s-]', "", text)
+    text = re.sub(r'[^a-zA-Z0-9@.+\s,\-:/]', "", text)
 
     # normalising new lines
     text = re.sub(r'\n+', '\n', text)
--- extraction/tempCodeRunnerFile.py
+++ extraction/tempCodeRunnerFile.py
@@ -1,1 +1,93 @@
-from extraction.cleaning import clean_text, 
+import os
+import re
+import fitz
+import pdfplumber
+from docx import Document
+import pypandoc
+from cleaning import clean_text
+
+# getting the extension type of the file
+ 
+file = "/Users/kshiraj/Downloads/resumes2/ResumeAryanSingh.pdf"
+ext = os.path.splitext(file)[1].lower()
+
+open("output.txt", "w").close()
+
+# extracting text from pdf files 
+
+if ext == '.pdf':
+    try:
+        doc = fitz.open(file)
+        with open("output.txt","a",encoding="utf8") as text:
+            for page in doc:
+                raw_data = page.get_text("text")
+                if raw_data:
+                    clean_data = clean_text(raw_data)
+                    text.write(clean_data + "\n")
+
+    except:
+        with pdfplumber.open(file) as pdf:
+            with open("output.txt","a",encoding="utf8") as text:
+                
+                for page in pdf.pages:
+                    content = page.extract_text()   
+
+                    if content:
+                        text.write(content + "\n")
+                    tables = page.extract_tables()
+
+                    for table in tables:
+                        for row in table:
+                            row_text = " | ".join(str(cell) if cell else "" for cell in row)
+                            text.write(row_text + "\n")
+                
+                     
+# extracting data from a docx file
+
+elif ext == '.docx':
+    doc = Document(file)
+
+    with open("output.txt","a", encoding="utf8") as f:
+        for para in doc.paragraphs:
+            f.write(para.text + "\n")
+
+        for table in doc.tables:
+            for row in table.rows:
+                row_text = " | ".join(cell.text for cell in row.cells)
+                f.write(row_text + "\n")
+
+
+# extracting files from a doc file
+
+elif ext == '.doc':
+    doc_file = file.replace(".doc",".docx")
+    
+    pypandoc.convert_file(file,"docx",outputfile=doc_file)
+
+    doc = Document(doc_file)
+
+    with open("output.txt","a", encoding="utf8") as f:
+        for para in doc.paragraphs:
+            f.write(para.text + "\n")
+
+        for table in doc.tables:
+            for row in table.rows:
+                row_text = " | ".join(cell.text for cell in row.cells)
+                f.write(row_text + "\n")
+
+
+else:
+    print("Unsupported file type")
+
+
+# reading the extracted text from the 
+
+print("\n <--- Extracted text: ---> \n")
+
+with open("output.txt","r", encoding="utf8") as f:
+    data = f.readlines()
+    for line in data:
+        print(line)
+print("\n <--- Extraction Ended ---> \n")
+
+
